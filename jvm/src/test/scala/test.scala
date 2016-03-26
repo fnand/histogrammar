53cb5cf48395dd4e48c837a8a7fd408d61e8868b
Jim Pivarski <jpivarski@gmail.com>
2016-03-26 02:26:16 -0500

    Working type logic for Binned(Count)

--- jvm/src/test/scala/test.scala
+++ jvm/src/test/scala/test.scala
@@ -8,6 +8,30 @@ import org.dianahep.histogrammar._
 
 class DefaultSuite extends FlatSpec with Matchers {
   "stuff" must "work" in {
-    println("here")
+    // def histogram[DATUM](num: Int, low: Double, high: Double, key: Weighted[DATUM] => Double, weighting: Weighting[Weighted[DATUM]] = unweighted) = Binned(Count[DATUM], num, low, high, key, weighting)
+
+    case class Datum(one: Int, two: Double, three: String)
+
+    // val hist = histogram(10, 5.0, 15.0, {d: Datum => d.one + d.two})
+
+    val hist = Binned(Count[Datum], 10, 5.0, 15.0, {d: Datum => d.one + d.two})
+    println(hist)
+
+    hist(Datum(3, 3.3, "three"))
+    println(hist)
+
+    hist(Datum(3, 3.3, "three"))
+    println(hist)
+
+    hist(Datum(6, 6.6, "four"))
+    println(hist)
+
+    hist(Datum(6, 6.6, "four"))
+    println(hist)
+
+    hist(Datum(6, 6.6, "four"))
+    println(hist)
+
+
   }
 }
