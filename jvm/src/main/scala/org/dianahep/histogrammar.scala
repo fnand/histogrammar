5aefbb3525af58e97ddda725ce0381c99b41ac71
Jim Pivarski <jpivarski@gmail.com>
2016-03-25 21:15:54 -0500

    First checkin.

---
+++ jvm/src/main/scala/org/dianahep/histogrammar.scala
@@ -0,0 +1,5 @@
+package org.dianahep
+
+package histogrammar {
+
+}
---
+++ jvm/src/test/scala/test.scala
@@ -0,0 +1,13 @@
+package test.scala.histogrammar
+
+import org.scalatest.FlatSpec
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.Matchers
+
+import org.dianahep.histogrammar._
+
+class DefaultSuite extends FlatSpec with Matchers {
+  "stuff" must "work" in {
+    println("here")
+  }
+}
