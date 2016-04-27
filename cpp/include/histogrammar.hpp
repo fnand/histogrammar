4ed29fb91f58b34ecda9a589b8a8dba4edff5f69
Jim Pivarski <jpivarski@gmail.com>
2016-04-27 04:43:25 -0500

    Binning compiles

--- cpp/include/histogrammar.hpp
+++ cpp/include/histogrammar.hpp
@@ -15,8 +15,12 @@
 #ifndef HISTOGRAMMAR_HPP
 #define HISTOGRAMMAR_HPP
 
-#include <string>
+#include <algorithm>
+#include <math.h>
 #include <memory>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
 #include "json.hpp"
 
@@ -148,10 +152,109 @@ namespace histogrammar {
 
   //////////////////////////////////////////////////////////////// Bin/Binned/Binning
 
-  // class Binning<DATUM> : public Container<CONTAINER>, Aggregation<DATUM> {
-  // public:
+  // class Binned;
+  // template <typename DATUM> class Binning;
 
+  // class Bin : public Factory {
+  // public:
+  //   std::string name() { return "Bin"; }
+  //   static std::unique_ptr<Binned> ed(double low, double high, double entries, std::vector<std::unique_ptr<> > values);
+  //   template <typename DATUM>
+  //   static std::unique_ptr<Binning<DATUM> > ing(std::function<double(DATUM)> quantity, std::function<double(DATUM)> selection = makeUnweighted<DATUM>());
   // };
+
+  template <typename V>
+  class Binned : public Container<Binned<V> > {
+    friend class Bin;
+  protected:
+    Binned(double low, double high, double entries, std::vector<std::unique_ptr<V> > values) : low_(low), high_(high), entries_(entries), values_(values) {
+      if (low >= high)
+        throw std::invalid_argument(std::string("low (") + std::to_string(low) + std::string(") must be less than high (") + std::to_string(high) + std::string(")"));
+      if (values.size() < 1)
+        throw std::invalid_argument(std::string("values must have at least one element"));
+      if (entries < 0.0)
+        throw std::invalid_argument(std::string("entries (") + std::to_string(entries) + std::string(") cannot be negative"));
+    }
+    double low_;
+    double high_;
+    double entries_;
+    std::vector<std::unique_ptr<V> > values_;
+  public:
+    int num() { return values_.size(); }
+    double low() { return low_; }
+    double high() { return high_; }
+    double entries() { return entries_; }
+    std::vector<std::unique_ptr<V> > values() { return values_; }
+
+    int bin(double x) {
+      if (under(x)  ||  over(x)  ||  nan(x))
+        return -1;
+      else
+        return (int)floor(num() * (x - low_) / (high_ - low_));
+    }
+
+    bool under(double x) {
+      return !isnan(x)  &&  x < low;
+    }
+    bool over(double x) {
+      return !isnan(x)  &&  x >= high;
+    }
+    bool nan(double x) {
+      return isnan(x);
+    }
+
+    std::vector<int> indexes() {
+      std::vector<int> out(num());
+      std::iota(out.begin(), out.end(), 0);
+      return out;   // should be NRVO-optimized, not a copy, right?
+    }
+    std::pair<double, double> range(int index) {
+      return std::pair<double, double>((high_ - low_) * index / num() + low_, (high_ - low_) * (index + 1) / num() + low_);
+    }
+    std::unique_ptr<V> at(int index) { return values_(index); }
+
+    std::unique_ptr<Binned<V> > zero() {
+      return std::unique_ptr<Binned<V> >(new Binned<V>(low_, high_, entries_, std::transform(values_.begin(), values_.end(), [](std::unique_ptr<V> v){v->zero();})));
+    }
+    std::unique_ptr<Binned<V> > plus(std::unique_ptr<Binned<V> > &that) {
+      if (this->low() != that->low())
+        throw std::invalid_argument(std::string("cannot add Binned because low differs (") + std::to_string(this->low()) + std::string(" vs ") + std::string(that->low()) + std::string(")"));
+      if (this->high() != that->high())
+        throw std::invalid_argument(std::string("cannot add Binned because high differs (") + std::to_string(this->high()) + std::string(" vs ") + std::string(that->high()) + std::string(")"));
+      if (this->num() != that->num())
+        throw std::invalid_argument(std::string("cannot add Binned because number of values differs (") + std::to_string(this->num()) + std::string(" vs ") + std::string(that->num()) + std::string(")"));
+
+      std::vector<std::unique_ptr<V> > newvalues(num());
+      for (int i = 0;  i < num();  i++)
+        newvalues[i] = this->at(i).plus(that->at(i));
+
+      return std::unique_ptr<Binned<V> >(new Binned<V>(low_, high_, entries_, newvalues));
+    }
+  };
+
+  // template <typename DATUM>
+  // class Binning : public Binned, Aggregation<DATUM> {
+  //   friend class Bin;
+  // private:
+  //   Binning(std::function<double(DATUM)> quantity, std::function<double(DATUM)> selection) : Binned(0.0, 0.0), quantity(quantity), selection(selection) { }
+  // public:
+  //   const std::function<double(DATUM)> quantity;
+  //   const std::function<double(DATUM)> selection;
+  //   void fill(DATUM datum, double weight = 1.0) {
+  //     double w = weight * selection(datum);
+  //     if (w > 0.0) {
+  //       double q = quantity(datum);
+  //       entries_ += w;
+  //       sum_ += q * w;
+  //     }
+  //   }
+  // };
+
+  // std::unique_ptr<Binned> Bin::ed(double entries, double sum) { return std::unique_ptr<Binned>(new Binned(entries, sum)); }
+
+  // template <typename DATUM>
+  // std::unique_ptr<Binning<DATUM> > Bin::ing(std::function<double(DATUM)> quantity, std::function<double(DATUM)> selection) { return std::unique_ptr<Binning<DATUM> >(new Binning<DATUM>(quantity, selection)); }
+
 
 }
 
