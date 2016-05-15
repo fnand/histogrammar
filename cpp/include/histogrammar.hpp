cfa188ff89f9c48f8bef596ec4e8663cc16548a2
Jim Pivarski <jpivarski@gmail.com>
2016-05-15 16:50:22 -0500

    update to version 0.7-prerelease

--- cpp/include/histogrammar.hpp
+++ cpp/include/histogrammar.hpp
@@ -29,7 +29,7 @@ using json = nlohmann::json;
 namespace histogrammar {
   //////////////////////////////////////////////////////////////// utilities
 
-  std::string VERSION = "0.6";
+  std::string VERSION = "0.7-prerelease";
 
   template <typename DATUM> std::function<double(DATUM)> makeUnweighted() {
     return [](DATUM datum){return 1.0;};
