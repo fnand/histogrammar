76945475116191cf6eb6b29aebffd48ba81687e3
Jim Pivarski <jpivarski@gmail.com>
2016-05-24 16:05:08 -0500

    pattern set by Sum

--- test-generator/TEMPLATES/PurePython.py
+++ test-generator/TEMPLATES/PurePython.py
@@ -21,17 +21,41 @@ import unittest
 from histogrammar import *
 from histogrammar.histogram import Histogram
 
+class Struct(object):
+    def __init__(self, x, y, z, w):
+        self.bool = x
+        self.int = y
+        self.double = z
+        self.string = w
+    def __repr__(self):
+        return "Struct({}, {}, {}, {})".format(self.bool, self.int, self.double, repr(self.string))
+
 def ed(x):
     return Factory.fromJson(x.toJson())
 
 class TestEverything(unittest.TestCase):
-    class Struct(object):
-        def __init__(self, x, y, z, w):
-            self.bool = x
-            self.int = y
-            self.double = z
-            self.string = w
-        def __repr__(self):
-            return "Struct({}, {}, {}, {})".format(self.bool, self.int, self.double, self.string)
+    def assertAlmostEqualJSON(self, x, y):
+        if isinstance(x, dict) and isinstance(y, dict):
+            if set(x.keys()) == set(y.keys()):
+                for k in x.keys():
+                    self.assertAlmostEqualJSON(x[k], y[k])
+            else:
+                raise AssertionError("keys {} are not equal to keys {}".format(sorted(x.keys()), sorted(y.keys())))
+
+        elif isinstance(x, list) and isinstance(y, list):
+            if len(x) == len(y):
+                for xi, yi in zip(x, y):
+                    self.assertAlmostEqualJSON(xi, yi)
+            else:
+                raise AssertionError("length of {} is not equal to length of {}".format(x, y))
+
+        elif isinstance(x, basestring) and isinstance(y, basestring):
+            self.assertEqual(x, y)
+
+        elif isinstance(x, (int, long, float)) and isinstance(y, (int, long, float)):
+            self.assertAlmostEqual(x, y)
+
+        else:
+            self.assertEqual(x, y)
 
 {{TESTS}}
